2535ec9804883c96d0bb366d7b25f7dc3ab8909f
FredyMojica <fremo22@gmail.com>
2023-08-23 19:26:09 -0500

    Add charts changes

---
+++ app/charts.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+
+def generate_bar_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    plt.savefig('bar.png')
+    plt.close()
+
+def generate_pie_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    plt.savefig('pie.png')
+    plt.close()
+
+if __name__ == "__main__":
+    labels = ["a", "b", "c"]
+    values = [100, 200, 80]
+    generate_bar_chart(labels, values)
+    generate_pie_chart(labels, values)
+
